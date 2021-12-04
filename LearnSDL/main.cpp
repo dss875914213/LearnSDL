27d5c58b595091205b35eddb4b12133ba0ae829a
dss <dss875914213@163.com>
2021-12-05 00:03:08 +0800

    使用另外一个教程的 hello world http://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php

--- LearnSDL/main.cpp
+++ LearnSDL/main.cpp
@@ -1,69 +1,102 @@
 #include "SDL/SDL.h"
 #include <iostream>
+#include <string>
+using namespace std;
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();// Starts up SDL and creates window
+bool loadMedia();// Loads media
+void close();// Frees media and shuts down SDL
+
+SDL_Window* gWindow = NULL;// The window we'll be rendering to
+SDL_Surface* gScreenSurface = NULL; // The surface contained by the window
+SDL_Surface* gHelloWorld = NULL;// The image we will load and show on the screen
+
+bool init()
+{
+	bool success = true; // Initialization flag
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+		success = false;
+	}
+	else
+	{
+		// Create window 
+		gWindow = SDL_CreateWindow("SDL Tutorial1", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+bool loadMedia()
+{
+	// Loading success flag
+	bool success = true;
+	//Load splash image ¼ÓÔØÆô¶¯»­Ãæ
+	string path = "../Resource/Hello.bmp";
+	gHelloWorld = SDL_LoadBMP(path.c_str());
+	if (gHelloWorld == NULL)
+	{
+		cout << "Unable to load image " << path << "! SDL Error: " << SDL_GetError() << endl;
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	//Quit SDL subsystems
+	SDL_Quit();
+}
 
 int main(int argc, char* args[])
 {
-	// Starting SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	//Start up SDL and create window
+	if (!init())
 	{
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+		cout << "Failed to initialize!" << endl;
 	}
-
-	// Opening a window
-	SDL_Window* win = SDL_CreateWindow("Hello world!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	if (win == nullptr)
+	else
 	{
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
+		//Load media
+		if (!loadMedia())
+		{
+			cout << "Failed to load media!" << endl;
+		}
+		else
+		{
+			// Apply the image
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			//Update the surface
+			SDL_UpdateWindowSurface(gWindow);
+			//Wait two seconds
+			SDL_Delay(2000);
+		}
 	}
+	// Free resources and close SDL
+	close();
 
-	// Creating a Renderer
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == nullptr)
-	{
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	// Loading a Bitmap Image
-	SDL_Surface* bmp = SDL_LoadBMP("../Resource/hello.bmp");
-	if (bmp == nullptr)
-	{
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-	SDL_FreeSurface(bmp);
-	if (tex == nullptr)
-	{
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	// Drawing the Texture
-	for (int i = 0; i < 3; i++)
-	{
-		SDL_RenderClear(ren);
-		SDL_RenderCopy(ren, tex, NULL, NULL);
-		SDL_RenderPresent(ren);
-		SDL_Delay(1000);
-	}
-
-	// Cleaning Up
-	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+	return 0;
 }
+
 
